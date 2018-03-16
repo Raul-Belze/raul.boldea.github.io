701f5c9e523cfecd21145869fef4e2a5707080dc
Raul-Belze <juniorboy94@yahoo.com>
2018-03-16 18:33:11 +0200

    Zecimale

--- x3D/orientare.js
+++ x3D/orientare.js
@@ -2,5 +2,5 @@ window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e)
 {
-	document.getElementById("id_orientation").innerHTML = e.alpha + " " + e.beta + " " + e.gamma;
+	document.getElementById("id_orientation").innerHTML = e.alpha.toFixed(2) + " " + e.beta.toFixed(2) + " " + e.gamma.toFixed(2);
 }
